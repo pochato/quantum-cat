f8cc220295c8125a0dec86b9a2bb91ab5566f6c9
dana-futa <128000092+dana-futa@users.noreply.github.com>
2023-12-05 22:43:36 -0700

    DF - making this identical to astro github pages template found here https://github.com/withastro/github-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://pochato.github.io',
+    // site: 'https://pochato.github.io',
     // base: '/quantum-cat',
 });
